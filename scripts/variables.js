eebde3265ca1a348d67db21e22e970bf614871e8
idevourkittens <bbdxd123456@gmail.com>
2024-08-26 15:55:30 -0600

    nut

--- scripts/variables.js
+++ scripts/variables.js
@@ -38,6 +38,8 @@ let favCandies = [
     "Chocolate"
 ];
 
+favCandies.length;
+
 console.log("Name?", firstName, lastName, "Age?", age, "IQ ",intelligenceQuotient, 
     "Max IQ is ", MAX_IQ, "Min IQ is ", MIN_IQ, "Am I silly? The answer is ", silly,
     "My cat is ", myPet, "My favorite candies are ", favCandies);
