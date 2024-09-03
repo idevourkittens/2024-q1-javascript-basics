f2fb6a971688a9b8f52c32a8a5968a994bd1b6ff
idevourkittens <bbdxd123456@gmail.com>
2024-09-03 16:19:11 -0600

    nick hit me

--- scripts/conditionals.js
+++ scripts/conditionals.js
@@ -1,1 +1,15 @@
 //@ts-check
+
+let isTheSkyBlue = false;
+let isTheSunShining = true;
+
+if (isTheSkyBlue) {
+    console.log("Day Time!!!");
+} else if (isTheSunShining && !isTheSkyBlue) {
+    let cloudColor = "grey!!!";
+    console.log("It be stormy and da clouds be", cloudColor);
+} else {
+    console.log("Night Time!!!");
+} 
+
+console.log(isTheSkyBlue, isTheSunShining);
